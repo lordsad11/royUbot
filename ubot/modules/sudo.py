b8cf65a645a672b4ee2421907a06a1763fefe5c5
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-22 01:22:38 +0700

    Update sudo.py

--- ubot/modules/sudo.py
+++ ubot/modules/sudo.py
@@ -3,20 +3,6 @@ from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
  
  
- __MODULE__ = "Sudo"
-__HELP__ = f"""
-<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴜᴅᴏ 』</b>
-
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>addsudo</code>
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴜsᴇʀ ᴋᴇ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ sᴜᴅᴏ
-
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>delsudo</code>
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs ᴜsᴇʀ ᴅᴀʀɪ ᴅᴀғᴛᴀʀ sᴜᴅᴏ
-  
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>getsudo</code>
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ ᴅᴀғᴛᴀʀ sᴜᴅᴏ
-"""
- 
 @PY.UBOT("addsudo")
 async def _(client, message):
      await addsudo_cmd(client, message)
