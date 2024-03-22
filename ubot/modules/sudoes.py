252d760bd0246ef6a2582347a2d242269cec025c
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-23 02:13:43 +0700

    Update sudoes.py

--- ubot/modules/sudoes.py
+++ ubot/modules/sudoes.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-from ubot.helper.PyroHelpers import get_ub_chats
+#from ubot.helper.PyroHelpers import get_ub_chats
 from ubot.modules.basic.profile import extract_user, extract_user_and_reason
 from ubot import SUDO_USER
 from ubot import OWNER_ID
