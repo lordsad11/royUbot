0dafba89246e43d949fda46f80524b477bb6b1c3
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-04 15:40:31 +0700

    Update vctools.py

--- ubot/modules/vctools.py
+++ ubot/modules/vctools.py
@@ -30,7 +30,7 @@ from pyrogram.raw.functions.messages import GetFullChat
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import Message
-from pytgcalls.exceptions import AlreadyJoinedError
+#from pytgcalls.exceptions import AlreadyJoinedError
 from pytgcalls.types.input_stream import InputAudioStream, InputStream
 
 from ubot import *
