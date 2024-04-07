a502a43d550d9cee1ced207ca2361aa55d7f2aa1
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-07 09:44:46 +0700

    Update vctools.py

--- ubot/modules/vctools.py
+++ ubot/modules/vctools.py
@@ -1,17 +1,51 @@
-from . import *
+from asyncio import sleep
+from contextlib import suppress
+from random import randint
+from typing import Optional
 
+from pyrogram import Client, enums, filters
+from pyrogram.raw.functions.channels import GetFullChannel
+from pyrogram.raw.functions.messages import GetFullChat
+from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
+from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
+from pyrogram.types import Message
 
-__MODULE__ = "Joinvc"
-__HELP__ = """
-Bantuan Untuk Joinvc
+from ubot import *
+from pytgcalls import GroupCallFactory 
 
-• Perintah: <code>{0}leavevc</code>
-• Penjelasan: Untuk meninggalkan voice chat grup.
+MODULE = "vctools"
+HELP = f"""
+✘ Bantuan Untuk Voice Chat
 
-• Perintah: <code>{0}joinvc</code>
-• Penjelasan: Untuk bergabung voice chat grup.
+๏ Perintah: <code>startvc</code>
+◉ Penjelasan: Untuk memulai voice chat grup.
+
+๏ Perintah: <code>stopvc</code>
+◉ Penjelasan: Untuk mengakhiri voice chat grup.
+           
+๏ Perintah: <code>joinvc</code>
+◉ Penjelasan: Untuk bergabung voice chat grup.
+
+๏ Perintah: <code>leavevc</code>
+◉ Penjelasan: Untuk meninggalkan voice chat grup.
 """
 
+
+async def get_group_call(
+    client: Client, message: Message, err_msg: str = ""
+) -> Optional[InputGroupCall]:
+    chat_peer = await client.resolve_peer(message.chat.id)
+    if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
+        if isinstance(chat_peer, InputPeerChannel):
+            full_chat = (await client.send(GetFullChannel(channel=chat_peer))).full_chat
+        elif isinstance(chat_peer, InputPeerChat):
+            full_chat = (
+                await client.send(GetFullChat(chat_id=chat_peer.chat_id))
+            ).full_chat
+        if full_chat is not None:
+            return full_chat.call
+    await eor(message, f"No group call Found {err_msg}")
+    return False
 
 list_data = []
 
@@ -33,7 +67,7 @@ def get_list():
     return msg
 
 
-@PY.UBOT("joinvc")
+@PY.UBOT("joinvc", sudo=True)
 async def _(client, message):
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
     text = f"• <b>[{client.me.first_name} {client.me.last_name or ''}](tg://user?id={client.me.id})</b> | <code>{chat_id}</code>"
@@ -41,24 +75,22 @@ async def _(client, message):
         await client.group_call.start(chat_id, join_as=client.me.id)
     except Exception as e:
         return await message.reply(f"ERROR: {e}")
-    await message.reply("<b>Si Jamet Naek Ke Pohon</b>")
+    await message.reply("<b>izin parkir puh</b>")
     await asyncio.sleep(5)
     await client.group_call.set_is_mute(True)
     add_list(client.me.id, text)
 
 
-@PY.UBOT("leavevc")
+@PY.UBOT("leavevc", sudo=True)
 async def _(client, message):
     try:
         await client.group_call.stop()
     except Exception as e:
         return await message.reply(f"ERROR: {e}")
     remove_list(client.me.id)
-    return await message.reply("<b>Si Jamet Turun Dari Pohon</b>")
+    return await message.reply("<b>izin turun puh</b>")
 
 
 @PY.UBOT("listos")
 async def _(client, message):
     await message.reply(get_list())
-
-           
