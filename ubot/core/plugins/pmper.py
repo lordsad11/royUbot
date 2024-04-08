ef56997034b854cd4382112126bdac2c3ae64e13
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-09 04:44:19 +0700

    pmper.py

--- ubot/core/plugins/pmper.py
+++ ubot/core/plugins/pmper.py
@@ -13,7 +13,7 @@ flood = {}
 flood2 = {}
 
 DEFAULT_TEXT = """
-<b>Saya adalah ᴀɪᴜ ᴜʙᴏᴛ yang menjaga Room Chat Ini . Jangan Spam Atau Anda Akan Diblokir Otomatis.</b>
+<b>Saya adalah Pacar kamu yang menjaga Room Chat Ini . Jangan Spam Atau Anda Akan Diblokir Otomatis.</b>
 """
 
 PM_WARN = """
