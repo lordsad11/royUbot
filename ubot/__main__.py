118ffdac52f65a85d06370e4d45323b17a60239c
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-09 04:20:23 +0700

    __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -20,10 +20,8 @@ async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
-        await ubot_.join_chat("aiusupportt")
-        await ubot_.join_chat("MusiknyaTelegram")
-        await ubot_.join_chat("AlterBaseChat")
-        await ubot_.join_chat("VicidiorSupport")
+        await ubot_.join_chat("gwehnc")
+        await ubot_.join_chat("fakeSupportt")
     except asyncio.TimeoutError:
         #await remove_ubot(user_id)
         await add_prem(user_id)
